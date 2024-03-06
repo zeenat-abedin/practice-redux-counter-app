a2306eebd7bb1164e3629a5d7a8e45f962da2015
zeenat <zeenu.23@gmail.com>
2024-03-06 17:29:29 +0530

    add CSS styles and state to Counter component

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './Counter.css'
 
 function Counter() {
+  let counter = 0
   return (
     <div className="counter-app">
           <h1>Counter Application</h1>
           <div className="counter">
-              <button className="increment">Increment</button>
+        <button className="increment">Increment</button>
+        <span>{ counter }</span>
               <button className="decrement">Decrement</button>
           </div>
     </div>
